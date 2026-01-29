ea4b3e36bb7d0b4fefbfd417bc91b449364a74d9
Mohammad Durrani <mdurrani808@gmail.com>
2026-01-29 14:02:53 -0500

    added todo

--- src/subsystems/navigation/athena_planner/launch/navigation.launch.py
+++ src/subsystems/navigation/athena_planner/launch/navigation.launch.py
@@ -9,6 +9,8 @@ from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
+    
+    # TODO: Re-enable the map functionality once the localizer works. Also needs corresponding changes in the Nav2 Cconfig file.
     #athena_map_share = get_package_share_directory('athena_map')
     #dem_launch = os.path.join(athena_map_share, 'launch', 'dem_costmap.launch.py')
 
